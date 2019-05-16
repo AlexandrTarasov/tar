0d0e1cd65051ac335889e5bad9ac5afceb5db175
ATarasov <usproinfo@gmail.com>
2019-05-16 23:24:04 +0300

    editing admin side

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,14 +3,14 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-12">
             <div class="card">
-                <div class="card-header">Dashboard</div>
+				<div class="card-header"><a href="">categoies</a>, <a href="">posts</a>, <a href="">tags</a> </div>
 
                 <div class="card-body">
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+                            <!-- {{ session('status') }} -->
                         </div>
                     @endif
 
