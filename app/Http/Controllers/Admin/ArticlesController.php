976069da723c9d06202011890738e2c4bd26158b
ATarasov <usproinfo@gmail.com>
2019-05-21 22:09:10 +0300

    added text editor in admin panel summernote installed with npm, added paginating

--- app/Http/Controllers/Admin/ArticlesController.php
+++ app/Http/Controllers/Admin/ArticlesController.php
@@ -16,7 +16,7 @@ class ArticlesController extends Controller
     public function index()
     {
 		return view('admin.articles.index',[
-			'articles'=>Article::orderBy('created_at','desc')->paginate(10)
+			'articles'=>Article::orderBy('created_at','desc')->paginate(4)
 		]);
     }
 
--- resources/views/admin/articles/index.blade.php
+++ resources/views/admin/articles/index.blade.php
@@ -1,15 +1,20 @@
 @extends('admin.app')
 @section('content')
 <div class="container">
+	<form method="post">
 		@forelse($articles as $article)
+		<div class=""  style='margin-bottom: 25px;'>
 			<input type="text" value ="{{$article->title}}"></br>
-			<textarea class="form-control" id="exampleFormControlTextarea1" rows="3">{{$article->desc}}</textarea>
+			<textarea class="form-control summernote" id="summernote" 
+			rows="3">{{$article->text}}</textarea>
+		</div>
 		@empty
 			<tr>
 				<td colspan="3" class="text-center"><h1>NO data</h1></td>
 			</tr>
 		@endforelse
-	</tbody>
-	<tbody>
+	</form>
+	{{ $articles->links() }}
 </div>
+
 @endsection
