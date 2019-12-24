c6628d1bd4aa54a6333fbd5fd253d139672f502c
ATarasov <usproinfo@gmail.com>
2019-12-24 18:54:38 +0200

    corrected del button

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -8,22 +8,24 @@
         </div>
     @endif
 	@forelse($posts as $post)
-		<form onsubmit="if(confirm('Del?')){ return true; }else{ return false; }" action="{{route('admin.posts.destroy', $post)}}" method="POST">
-			<input type="hidden" name="_method" value="DELETE">
-			{{ csrf_field() }}
-			<button type="submit" style="float:right;" class="btn btn-danger">del</button>
-		</form>
-		<form action="{{route('admin.posts.update', $post->id )}}" method="post">
-			{{ csrf_field() }}
-			<div class=""  style='margin-bottom: 25px;'>
-				<!-- <input type="hidden" name="_method" value="put"> -->
-				 {{ method_field('PUT') }}
-				<input name="title" type="text" value ="{{$post->title}}"></br>
-				<textarea name="text" class="form-control summernote" id="summernote" 
-				rows="3">{{$post->text}}</textarea>
-				<input class="btn btn-primary" type="submit" value="Save">
-			</div>
-		</form>
+
+		<div class=""  style='margin-bottom: 25px;'>
+			<form action="{{route('admin.posts.update', $post->id )}}" method="post">
+				{{ csrf_field() }}
+					<!-- <input type="hidden" name="_method" value="put"> -->
+					 {{ method_field('PUT') }}
+					<input name="title" type="text" value ="{{$post->title}}"></br>
+					<textarea name="text" class="form-control summernote" id="summernote" 
+					rows="3">{{$post->text}}</textarea>
+					<input class="btn btn-primary" type="submit" value="Save">
+					<input type="submit"  style="float:right;" class="btn btn-danger" name="_method" value="DELETE" onclick="if(confirm('Del?')){ return true; }else{ return false; }">
+			</form>
+			<!--form onsubmit="if(confirm('Del?')){ return true; }else{ return false; }" action="{{route('admin.posts.destroy', $post)}}" method="POST">
+				<input type="hidden" name="_method" value="DELETE">
+				{{ csrf_field() }}
+				<button type="submit" style="float:right;" class="btn btn-danger">del</button>
+			</form-->
+		</div>
 
 		
 		@empty
